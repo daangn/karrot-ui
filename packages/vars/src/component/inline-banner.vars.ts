15ac5c8a9633323f42e6aac3516bfa67d6c21a5c
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-11-01 14:01:50 +0900

    feat(vars): generate `inline-banner`

---
+++ packages/vars/src/component/inline-banner.vars.ts
@@ -0,0 +1,159 @@
+export const vars = {
+  "base": {
+    "enabled": {
+      "root": {
+        "gap": "var(--seed-v3-unit-4)",
+        "paddingX": "var(--seed-v3-unit-4)",
+        "paddingY": "var(--seed-v3-unit-2\\.5)"
+      },
+      "content": {
+        "gap": "var(--seed-v3-unit-2)"
+      },
+      "prefixIconContainer": {
+        "size": "19px"
+      },
+      "prefixIcon": {
+        "size": "16px"
+      },
+      "xIcon": {
+        "size": "16px"
+      },
+      "label": {
+        "fontSize": "var(--seed-v3-font-size-100)",
+        "fontWeight": "var(--seed-v3-font-weight-medium)"
+      },
+      "link": {
+        "fontSize": "var(--seed-v3-font-size-50)",
+        "fontWeight": "var(--seed-v3-font-weight-regular)"
+      }
+    }
+  },
+  "variantSolidToneNeutral": {
+    "enabled": {
+      "root": {
+        "color": "var(--seed-v3-color-palette-gray-700)"
+      }
+    }
+  },
+  "variantSolidTonePositive": {
+    "enabled": {
+      "root": {
+        "color": "var(--seed-v3-color-bg-positive-solid)"
+      }
+    }
+  },
+  "variantSolidToneInformative": {
+    "enabled": {
+      "root": {
+        "color": "var(--seed-v3-color-bg-informative-solid)"
+      }
+    }
+  },
+  "variantSolidToneWarning": {
+    "enabled": {
+      "root": {
+        "color": "var(--seed-v3-color-bg-warning-solid)"
+      }
+    }
+  },
+  "variantSolidToneDanger": {
+    "enabled": {
+      "root": {
+        "color": "var(--seed-v3-color-bg-danger-solid)"
+      }
+    }
+  },
+  "variantSolid": {
+    "enabled": {
+      "prefixIcon": {
+        "color": "var(--seed-v3-color-fg-static-white)"
+      },
+      "label": {
+        "color": "var(--seed-v3-color-fg-static-white)"
+      },
+      "xIcon": {
+        "color": "var(--seed-v3-color-fg-static-white)"
+      }
+    }
+  },
+  "variantWeakToneNeutral": {
+    "enabled": {
+      "root": {
+        "color": "var(--seed-v3-color-bg-neutral-weak)"
+      },
+      "prefixIcon": {
+        "color": "var(--seed-v3-color-fg-neutral)"
+      },
+      "label": {
+        "color": "var(--seed-v3-color-fg-neutral)"
+      },
+      "xIcon": {
+        "color": "var(--seed-v3-color-fg-neutral)"
+      }
+    }
+  },
+  "variantWeakTonePositive": {
+    "enabled": {
+      "root": {
+        "color": "var(--seed-v3-color-bg-positive-weak)"
+      },
+      "prefixIcon": {
+        "color": "var(--seed-v3-color-fg-positive-contrast)"
+      },
+      "label": {
+        "color": "var(--seed-v3-color-fg-positive-contrast)"
+      },
+      "xIcon": {
+        "color": "var(--seed-v3-color-fg-positive-contrast)"
+      }
+    }
+  },
+  "variantWeakToneInformative": {
+    "enabled": {
+      "root": {
+        "color": "var(--seed-v3-color-bg-informative-weak)"
+      },
+      "prefixIcon": {
+        "color": "var(--seed-v3-color-fg-informative-contrast)"
+      },
+      "label": {
+        "color": "var(--seed-v3-color-fg-informative-contrast)"
+      },
+      "xIcon": {
+        "color": "var(--seed-v3-color-fg-informative-contrast)"
+      }
+    }
+  },
+  "variantWeakToneWarning": {
+    "enabled": {
+      "root": {
+        "color": "var(--seed-v3-color-bg-warning-weak)"
+      },
+      "prefixIcon": {
+        "color": "var(--seed-v3-color-fg-warning-contrast)"
+      },
+      "label": {
+        "color": "var(--seed-v3-color-fg-warning-contrast)"
+      },
+      "xIcon": {
+        "color": "var(--seed-v3-color-fg-warning-contrast)"
+      }
+    }
+  },
+  "variantWeakToneDanger": {
+    "enabled": {
+      "root": {
+        "color": "var(--seed-v3-color-bg-danger-weak)"
+      },
+      "prefixIcon": {
+        "color": "var(--seed-v3-color-fg-danger)"
+      },
+      "label": {
+        "color": "var(--seed-v3-color-fg-danger)"
+      },
+      "xIcon": {
+        "color": "var(--seed-v3-color-fg-danger)"
+      }
+    }
+  }
+}
