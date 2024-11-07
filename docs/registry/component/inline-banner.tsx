8e955a2f8cf8c25ff1bcde37537e02fc68dff09a
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-11-07 12:13:47 +0900

    feat: make inline banner to use `useDismissible` hook

--- docs/registry/component/inline-banner.tsx
+++ docs/registry/component/inline-banner.tsx
@@ -9,19 +9,28 @@ import {
   inlineBanner,
   type InlineBannerVariantProps,
 } from "@seed-design/recipe/inlineBanner";
+import { IconXmarkLine } from "@daangn/react-monochrome-icon";
+import {
+  useDismissible,
+  type DismissibleProps,
+} from "../hooks/use-dismissible";
 
 interface BaseInlineBannerProps extends InlineBannerVariantProps {
   titleText?: string;
   prefixIcon?: React.ReactNode;
 }
 
-interface DismissibleInlineBannerProps extends BaseInlineBannerProps {
+interface DismissibleInlineBannerProps
+  extends BaseInlineBannerProps,
+    DismissibleProps {
   tone?: Exclude<InlineBannerVariantProps["tone"], "danger">;
   dismissAriaLabel?: string;
   action?: never;
 }
 
-interface NondismissibleInlineBannerProps extends BaseInlineBannerProps {
+interface NondismissibleInlineBannerProps
+  extends BaseInlineBannerProps,
+    Partial<Record<keyof DismissibleProps, never>> {
   dismissAriaLabel?: never;
   action?: {
     onClick: React.MouseEventHandler<HTMLButtonElement>;
@@ -36,32 +45,6 @@ export type InlineBannerProps =
 type ReactInlineBannerProps = React.HTMLAttributes<HTMLDivElement> &
   InlineBannerProps;
 
-const XmarkLine = React.forwardRef<
-  SVGSVGElement,
-  React.SVGProps<SVGSVGElement>
->((props, ref) => (
-  // FIXME
-  // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
-  <svg
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    data-seed-icon="true"
-    data-seed-icon-version="0.0.0-alpha-20241022092043"
-    width="16"
-    height="16"
-    ref={ref}
-    {...props}
-  >
-    <g>
-      <path
-        d="M20.7071 4.70711C21.0976 4.31658 21.0976 3.68342 20.7071 3.29289C20.3166 2.90237 19.6834 2.90237 19.2929 3.29289L12 10.5858L4.70711 3.29289C4.31658 2.90237 3.68342 2.90237 3.29289 3.29289C2.90237 3.68342 2.90237 4.31658 3.29289 4.70711L10.5858 12L3.29289 19.2929C2.90237 19.6834 2.90237 20.3166 3.29289 20.7071C3.68342 21.0976 4.31658 21.0976 4.70711 20.7071L12 13.4142L19.2929 20.7071C19.6834 21.0976 20.3166 21.0976 20.7071 20.7071C21.0976 20.3166 21.0976 19.6834 20.7071 19.2929L13.4142 12L20.7071 4.70711Z"
-        fill="currentColor"
-      />
-    </g>
-  </svg>
-));
-
 export const InlineBanner = React.forwardRef<
   HTMLDivElement,
   ReactInlineBannerProps
@@ -75,6 +58,9 @@ export const InlineBanner = React.forwardRef<
       titleText,
       action,
       dismissAriaLabel,
+      defaultOpen,
+      isOpen: isPropOpen,
+      onDismiss,
       ...otherProps
     },
     ref,
@@ -83,6 +69,14 @@ export const InlineBanner = React.forwardRef<
 
     const rootRef = React.useRef<HTMLDivElement>(null);
     React.useImperativeHandle(ref, () => rootRef.current as HTMLDivElement);
+
+    const { isOpen, onDismissButtonClick } = useDismissible({
+      defaultOpen,
+      isOpen: isPropOpen,
+      onDismiss,
+    });
+
+    if (!isOpen) return null;
 
     return (
       <div
@@ -109,9 +103,9 @@ export const InlineBanner = React.forwardRef<
             type="button"
             aria-label={dismissAriaLabel}
             className={classNames.dismissButton}
-            onClick={() => rootRef.current?.remove()}
+            onClick={onDismissButtonClick}
           >
-            <XmarkLine className={classNames.xIcon} />
+            <IconXmarkLine className={classNames.xIcon} />
           </button>
         )}
         {action && (
