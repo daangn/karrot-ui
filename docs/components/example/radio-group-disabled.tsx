6a98d7e2e796e5e915fb109b62478521041ff82f
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-11-13 13:07:26 +0900

    docs: show selected & disabled case & remove logging

--- docs/components/example/radio-group-disabled.tsx
+++ docs/components/example/radio-group-disabled.tsx
@@ -2,7 +2,7 @@ import { RadioGroup, Radio } from "seed-design/ui/radio-group";
 
 export default function RadioGroupDisabled() {
   return (
-    <RadioGroup disabled>
+    <RadioGroup disabled value="officia">
       <Radio value="duis">duis</Radio>
       <Radio value="officia">officia</Radio>
       <Radio value="sint">sint</Radio>
