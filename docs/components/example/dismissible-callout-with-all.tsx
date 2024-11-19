2f6d93e75e071627003bf256e8e39c2679acaee0
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-11-19 19:43:58 +0900

    fix: fix invalid example
    (cherry picked from commit 96cf75c977a8468f43297b0df4c330a6423e2458)
    (cherry picked from commit 25ada5799b48d26f5869d5859ed4c7c3f19124f9)

--- docs/components/example/dismissible-callout-with-all.tsx
+++ docs/components/example/dismissible-callout-with-all.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { IconSparkle2Fill } from "@daangn/react-monochrome-icon";
 import { DismissibleCallout } from "seed-design/ui/dismissible-callout";
 
 export default function DismissibleCalloutWithAll() {
@@ -8,7 +7,6 @@ export default function DismissibleCalloutWithAll() {
     <DismissibleCallout
       dismissAriaLabel="닫기"
       variant="magic"
-      icon={<IconSparkle2Fill />}
       titleText="새로운 기능"
       linkLabel="시도해 보기"
       onLinkLabelClick={() => window.alert("Hello World")}
