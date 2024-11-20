9d4a775db10a734937a3f5ea8b131b7da19914ca
junghyeonsu <jung660317@naver.com>
2024-11-20 16:09:27 +0900

    feat(docs): 메인페이지 이미지를 변경해요

--- docs/app/(home)/page.tsx
+++ docs/app/(home)/page.tsx
@@ -2,18 +2,16 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="flex h-screen flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">SEED Design</h1>
-      <p className="text-fd-muted-foreground">
+    <main className="flex flex-col justify-center items-center text-center">
+      {/* <h1 className="mb-4 text-2xl font-bold">SEED Design</h1> */}
+      {/* <p className="text-fd-muted-foreground">
         You can open{" "}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
+        <Link href="/docs" className="text-fd-foreground font-semibold underline">
           /docs
         </Link>{" "}
         and see the documentation.
-      </p>
+      </p> */}
+      <img src="/cover.png" alt="SEED Design" className="mt-[60px] rounded-[26px] w-[80%]" />
     </main>
   );
 }
