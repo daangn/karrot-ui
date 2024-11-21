a0c1416abacb47611a47097a50a90cd11b52963d
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-11-21 14:19:33 +0900

    refactor: inline banner에 link prop을 추가하고 link inline banner를 제거해요

--- docs/components/example/inline-banner-with-all.tsx
+++ docs/components/example/inline-banner-with-all.tsx
@@ -3,12 +3,13 @@
 import { IconILowercaseSerifCircleFill } from "@daangn/react-monochrome-icon";
 import { InlineBanner } from "seed-design/ui/inline-banner";
 
-export default function InlineBannerWithIconAndTitleText() {
+export default function InlineBannerWithAll() {
   return (
     <InlineBanner
       variant="informativeWeak"
       icon={<IconILowercaseSerifCircleFill />}
       titleText="예약"
+      link={{ label: "자세히 보기", onClick: () => {} }}
     >
       다른 사람과 예약된 물품이 있어요.
     </InlineBanner>
