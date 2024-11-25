f0690a0147c4ee164a4ce2acb74ae7579d8049ee
malangcat <asher@daangn.com>
2024-11-25 22:11:25 +0900

    update cli

--- packages/rootage/cli/src/index.ts
+++ packages/rootage/cli/src/index.ts
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
-import { parse, stringifyTs } from "@seed-design/component-spec-core";
+import {
+  getJsonSchema,
+  type Model,
+  parseComponentSpecData,
+  stringifyComponentSpecTs,
+  validateModels,
+} from "@seed-design/rootage-core";
 import fs from "fs-extra";
 import path from "node:path";
 import YAML from "yaml";
@@ -9,29 +15,77 @@ import { createRequire } from "node:module";
 
 const require = createRequire(import.meta.url);
 
-const artifactsPath = require.resolve("@seed-design/component-spec-artifacts");
+const artifactsPath = require.resolve("@seed-design/rootage-artifacts");
 const artifactsDir = path.dirname(artifactsPath);
 
-const [, , dir = "./"] = process.argv;
+const [, , command, dir = "./"] = process.argv;
 
-async function write() {
-  const fileNames = await fs.readdir(artifactsDir);
-  const filesToRead = fileNames.filter((fileName) => fileName.endsWith(".yaml"));
+function readYAMLFilesSync(dir: string, fileList: string[] = []) {
+  const files = fs.readdirSync(dir);
 
-  return Promise.all(
-    filesToRead.map(async (name) => {
-      const content = await fs.readFile(path.join(artifactsDir, name), "utf-8");
-      const definition = YAML.parse(content);
-      const code = stringifyTs(parse(definition));
-      const writePath = path.join(process.cwd(), dir, `${name.split(".")[0]}.vars.ts`);
+  for (const file of files) {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
 
-      console.log("Writing", name, "to", writePath);
+    if (stat.isDirectory()) {
+      readYAMLFilesSync(filePath, fileList);
+    } else if (stat.isFile() && (path.extname(file) === ".yaml" || path.extname(file) === ".yml")) {
+      fileList.push(filePath);
+    }
+  }
 
-      fs.writeFileSync(writePath, code);
-    }),
-  );
+  return fileList;
 }
 
-write().then(() => {
-  console.log("Done");
-});
+async function writeComponentSpec() {
+  const filesToRead = readYAMLFilesSync(artifactsDir);
+  const fileContents: Model[] = await Promise.all(
+    filesToRead.map((name) => fs.readFile(name, "utf-8").then((res) => YAML.parse(res))),
+  );
+
+  const validationResult = validateModels(fileContents);
+
+  if (!validationResult.valid) {
+    console.error(validationResult.message);
+    process.exit(1);
+  }
+
+  const componentSpecFiles = fileContents.filter((model) => model.kind === "ComponentSpec");
+
+  for (const spec of componentSpecFiles) {
+    const code = stringifyComponentSpecTs(parseComponentSpecData(spec.data));
+    const writePath = path.join(process.cwd(), dir, `${spec.metadata.id}.vars.ts`);
+
+    console.log("Writing", spec.metadata.name, "to", writePath);
+
+    fs.writeFileSync(writePath, code);
+  }
+}
+
+async function writeJsonSchema() {
+  const filesToRead = readYAMLFilesSync(artifactsDir);
+  const fileContents = await Promise.all<Model>(
+    filesToRead.map((name) => fs.readFile(name, "utf-8").then((res) => YAML.parse(res))),
+  ).then((res) => res.filter((model) => model.kind === "Tokens"));
+
+  // FIXME: add model validation
+
+  const jsonSchema = getJsonSchema(fileContents);
+  const writePath = path.join(artifactsDir, "components", "schema.json");
+
+  console.log("Writing schema to", writePath);
+
+  fs.writeFileSync(writePath, jsonSchema);
+}
+
+if (command === "component-spec") {
+  writeComponentSpec().then(() => {
+    console.log("Done");
+  });
+}
+
+if (command === "json-schema") {
+  writeJsonSchema().then(() => {
+    console.log("Done");
+  });
+}
