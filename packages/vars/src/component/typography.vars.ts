b0a961551e58a0b26b6646149d12e2070c73823d
malangcat <asher@daangn.com>
2024-11-26 19:42:42 +0900

    add typography spec

---
+++ packages/vars/src/component/typography.vars.ts
@@ -0,0 +1,209 @@
+export const vars = {
+  "typeLabelSmallDefault": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t1)",
+        "lineHeight": "var(--seed-v3-line-height-t1)",
+        "fontWeight": "var(--seed-v3-font-weight-regular)"
+      }
+    }
+  },
+  "typeLabelSmallStrong": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t1)",
+        "lineHeight": "var(--seed-v3-line-height-t1)",
+        "fontWeight": "var(--seed-v3-font-weight-medium)"
+      }
+    }
+  },
+  "typeLabelSmallStronger": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t1)",
+        "lineHeight": "var(--seed-v3-line-height-t1)",
+        "fontWeight": "var(--seed-v3-font-weight-bold)"
+      }
+    }
+  },
+  "typeLabelMediumDefault": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t2)",
+        "lineHeight": "var(--seed-v3-line-height-t2)",
+        "fontWeight": "var(--seed-v3-font-weight-regular)"
+      }
+    }
+  },
+  "typeLabelMediumStrong": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t2)",
+        "lineHeight": "var(--seed-v3-line-height-t2)",
+        "fontWeight": "var(--seed-v3-font-weight-medium)"
+      }
+    }
+  },
+  "typeLabelMediumStronger": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t2)",
+        "lineHeight": "var(--seed-v3-line-height-t2)",
+        "fontWeight": "var(--seed-v3-font-weight-bold)"
+      }
+    }
+  },
+  "typeLabelLargeDefault": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t3)",
+        "lineHeight": "var(--seed-v3-line-height-t3)",
+        "fontWeight": "var(--seed-v3-font-weight-regular)"
+      }
+    }
+  },
+  "typeLabelLargeStrong": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t3)",
+        "lineHeight": "var(--seed-v3-line-height-t3)",
+        "fontWeight": "var(--seed-v3-font-weight-medium)"
+      }
+    }
+  },
+  "typeLabelLargeStronger": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t3)",
+        "lineHeight": "var(--seed-v3-line-height-t3)",
+        "fontWeight": "var(--seed-v3-font-weight-bold)"
+      }
+    }
+  },
+  "typeBodySmallDefault": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t4)",
+        "lineHeight": "var(--seed-v3-line-height-t4)",
+        "fontWeight": "var(--seed-v3-font-weight-regular)"
+      }
+    }
+  },
+  "typeBodySmallReadingDefault": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t4)",
+        "lineHeight": "var(--seed-v3-line-height-t5)",
+        "fontWeight": "var(--seed-v3-font-weight-regular)"
+      }
+    }
+  },
+  "typeBodySmallStrong": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t4)",
+        "lineHeight": "var(--seed-v3-line-height-t4)",
+        "fontWeight": "var(--seed-v3-font-weight-medium)"
+      }
+    }
+  },
+  "typeBodySmallStronger": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t4)",
+        "lineHeight": "var(--seed-v3-line-height-t4)",
+        "fontWeight": "var(--seed-v3-font-weight-bold)"
+      }
+    }
+  },
+  "typeBodyMediumDefault": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t5)",
+        "lineHeight": "var(--seed-v3-line-height-t5)",
+        "fontWeight": "var(--seed-v3-font-weight-regular)"
+      }
+    }
+  },
+  "typeBodyMediumReadingDefault": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t5)",
+        "lineHeight": "var(--seed-v3-line-height-t6)",
+        "fontWeight": "var(--seed-v3-font-weight-regular)"
+      }
+    }
+  },
+  "typeBodyMediumStrong": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t5)",
+        "lineHeight": "var(--seed-v3-line-height-t5)",
+        "fontWeight": "var(--seed-v3-font-weight-medium)"
+      }
+    }
+  },
+  "typeBodyMediumStronger": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t5)",
+        "lineHeight": "var(--seed-v3-line-height-t5)",
+        "fontWeight": "var(--seed-v3-font-weight-bold)"
+      }
+    }
+  },
+  "typeTitleSmallDefault": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t5)",
+        "lineHeight": "var(--seed-v3-line-height-t5)",
+        "fontWeight": "var(--seed-v3-font-weight-bold)"
+      }
+    }
+  },
+  "typeTitleMediumDefault": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t6)",
+        "lineHeight": "var(--seed-v3-line-height-t6)",
+        "fontWeight": "var(--seed-v3-font-weight-bold)"
+      }
+    }
+  },
+  "typeTitleLargeDefault": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t7)",
+        "lineHeight": "var(--seed-v3-line-height-t7)",
+        "fontWeight": "var(--seed-v3-font-weight-bold)"
+      }
+    }
+  },
+  "typeHeadingSmallDefault": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t8)",
+        "lineHeight": "var(--seed-v3-line-height-t8)",
+        "fontWeight": "var(--seed-v3-font-weight-bold)"
+      }
+    }
+  },
+  "typeHeadingMediumDefault": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t9)",
+        "lineHeight": "var(--seed-v3-line-height-t9)",
+        "fontWeight": "var(--seed-v3-font-weight-bold)"
+      }
+    }
+  },
+  "typeHeadingLargeDefault": {
+    "enabled": {
+      "root": {
+        "fontSize": "var(--seed-v3-font-size-t10)",
+        "lineHeight": "var(--seed-v3-line-height-t10)",
+        "fontWeight": "var(--seed-v3-font-weight-bold)"
+      }
+    }
+  }
+}
