38b7e2f2f034d93d7ebcc532fa9516d86370168c
junghyeonsu <jung660317@naver.com>
2024-11-26 17:07:42 +0900

    chore: 테스트

--- packages/react-headless/checkbox/src/useCheckbox.test.tsx
+++ packages/react-headless/checkbox/src/useCheckbox.test.tsx
@@ -1,4 +1,4 @@
-import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/vitest";
 import { cleanup, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { afterEach, describe, expect, it, vi } from "vitest";
