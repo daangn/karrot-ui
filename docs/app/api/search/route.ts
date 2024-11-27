7568720196e0fdfedd1faf4c67d9cda2ef8f6997
malangcat <asher@daangn.com>
2024-11-28 03:14:51 +0900

    fix: remove console.log

--- docs/app/api/search/route.ts
+++ docs/app/api/search/route.ts
@@ -4,18 +4,13 @@ import { createFromSource } from "fumadocs-core/search/server";
 // it should be cached forever
 export const revalidate = false;
 
-export const { staticGET: GET } = createFromSource(
-  source,
-  (page) => {
-    console.log(page.data.structuredData);
-    return {
-      title: page.data.title,
-      description: page.data.description,
-      url: page.url,
-      id: page.url,
-      structuredData: page.data.structuredData,
-      tag: page.slugs[0],
-    };
-  },
-  {},
-);
+export const { staticGET: GET } = createFromSource(source, (page) => {
+  return {
+    title: page.data.title,
+    description: page.data.description,
+    url: page.url,
+    id: page.url,
+    structuredData: page.data.structuredData,
+    tag: page.slugs[0],
+  };
+});
