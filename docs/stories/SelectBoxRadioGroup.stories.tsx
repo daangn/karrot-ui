abadaed2e73daef848136bd4f490a97c48aec1ac
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-12-03 16:12:26 +0900

    docs: add stories

---
+++ docs/stories/SelectBoxRadioGroup.stories.tsx
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { SelectBoxRadio, SelectBoxRadioGroup } from "seed-design/ui/select-box-radio-group";
+import { selectBoxVariantMap } from "@seed-design/recipe/selectBox";
+
+import { SeedThemeDecorator } from "./components/decorator";
+import { VariantTable } from "./components/variant-table";
+import { useState } from "react";
+
+const Component = () => {
+  const values = ["dolor", "magna", "sint"];
+  const [value, setValue] = useState(values[0]);
+
+  return (
+    <SelectBoxRadioGroup
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        gap: "16px",
+      }}
+      value={value}
+      onValueChange={setValue}
+    >
+      {values.map((value) => (
+        <SelectBoxRadio key={value} value={value} label={value} description={value} />
+      ))}
+    </SelectBoxRadioGroup>
+  );
+};
+
+const meta = {
+  component: SelectBoxRadioGroup,
+  decorators: [SeedThemeDecorator],
+} satisfies Meta<typeof SelectBoxRadioGroup>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const CommonStoryTemplate: Story = {
+  args: {
+    defaultValue: "dolor",
+  },
+  render: function Render(args) {
+    return <VariantTable Component={Component} variantMap={selectBoxVariantMap} {...args} />;
+  },
+};
+
+export const LightTheme = CommonStoryTemplate;
+
+export const DarkTheme = CommonStoryTemplate;
+
+export const FontScalingExtraSmall = CommonStoryTemplate;
+
+export const FontScalingExtraExtraExtraLarge = CommonStoryTemplate;
