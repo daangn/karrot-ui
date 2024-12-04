7cced1abd116289fad8ee53e09a3649741e36085
junghyeonsu <jung660317@naver.com>
2024-12-04 12:16:37 +0900

    feat: tabs overflow to visible

--- packages/recipe-generator/preset/src/tabs.recipe.ts
+++ packages/recipe-generator/preset/src/tabs.recipe.ts
@@ -40,7 +40,6 @@ const tabs = defineRecipe({
       height: "100%",
       flexShrink: 0,
       overflowY: "auto",
-      overflowX: "hidden",
     },
     indicator: {
       position: "absolute",
--- packages/recipe/CHANGELOG.md
+++ packages/recipe/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @seed-design/recipe
 
+## 0.0.0-alpha-20241204031604
+
+### Patch Changes
+
+- tabs overflowX to visible
+
 ## 0.0.0-alpha-20241203133940
 
 ### Patch Changes
