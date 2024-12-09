043b2eab1f7e620997344b55d65bd49849d88b07
malangcat <asher@daangn.com>
2024-12-09 19:10:54 +0900

    docs: update chip storybook

--- docs/stories/ControlChip.stories.tsx
+++ docs/stories/ControlChip.stories.tsx
@@ -4,7 +4,7 @@ import { ControlChip } from "seed-design/ui/control-chip";
 
 import { controlChipVariantMap } from "@seed-design/recipe/controlChip";
 import { VariantTable } from "./components/variant-table";
-import { IconBellFill } from "@daangn/react-monochrome-icon";
+import { IconBellFill, IconChevronDownFill } from "@daangn/react-monochrome-icon";
 import { SeedThemeDecorator } from "./components/decorator";
 
 const meta = {
@@ -20,32 +20,60 @@ const variantMap = { ...controlChipVariantMap, checked: ["false", "true"] };
 
 export const LightTheme: Story = {
   args: {
-    children: "Control Chip",
     prefixIcon: <IconBellFill />,
+    suffixIcon: <IconChevronDownFill />,
   },
-  render: (args) => <VariantTable Component={meta.component} variantMap={variantMap} {...args} />,
+  render: (args) => (
+    <VariantTable
+      Component={meta.component}
+      variantMap={variantMap}
+      render={({ layout }) => (layout === "withText" ? "Control Chip" : <IconBellFill />)}
+      {...args}
+    />
+  ),
 };
 
 export const DarkTheme: Story = {
   args: {
-    children: "Control Chip",
     prefixIcon: <IconBellFill />,
+    suffixIcon: <IconChevronDownFill />,
   },
-  render: (args) => <VariantTable Component={meta.component} variantMap={variantMap} {...args} />,
+  render: (args) => (
+    <VariantTable
+      Component={meta.component}
+      variantMap={variantMap}
+      render={({ layout }) => (layout === "withText" ? "Control Chip" : <IconBellFill />)}
+      {...args}
+    />
+  ),
 };
 
 export const FontScalingExtraSmall: Story = {
   args: {
-    children: "Control Chip",
     prefixIcon: <IconBellFill />,
+    suffixIcon: <IconChevronDownFill />,
   },
-  render: (args) => <VariantTable Component={meta.component} variantMap={variantMap} {...args} />,
+  render: (args) => (
+    <VariantTable
+      Component={meta.component}
+      variantMap={variantMap}
+      render={({ layout }) => (layout === "withText" ? "Control Chip" : <IconBellFill />)}
+      {...args}
+    />
+  ),
 };
 
 export const FontScalingExtraExtraExtraLarge: Story = {
   args: {
-    children: "Control Chip",
     prefixIcon: <IconBellFill />,
+    suffixIcon: <IconChevronDownFill />,
   },
-  render: (args) => <VariantTable Component={meta.component} variantMap={variantMap} {...args} />,
+  render: (args) => (
+    <VariantTable
+      Component={meta.component}
+      variantMap={variantMap}
+      render={({ layout }) => (layout === "withText" ? "Control Chip" : <IconBellFill />)}
+      {...args}
+    />
+  ),
 };
