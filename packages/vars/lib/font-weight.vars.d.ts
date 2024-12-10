1c4896750cc0ace3ef97d07528a9ac61191edfa9
malangcat <asher@daangn.com>
2024-12-10 11:02:24 +0900

    fix: vars package

--- packages/vars/lib/font-weight.vars.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const regular = "var(--seed-v3-font-weight-regular)";
-export declare const medium = "var(--seed-v3-font-weight-medium)";
-export declare const bold = "var(--seed-v3-font-weight-bold)";
