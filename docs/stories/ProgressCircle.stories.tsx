52f198373f17eb16ff93fb6f0eacd842281ce006
malangcat <asher@daangn.com>
2024-12-11 12:41:50 +0900

    test: add ProgressCircle stories

--- docs/stories/ProgressCircle.stories.tsx
+++ docs/stories/ProgressCircle.stories.tsx
@@ -15,13 +15,52 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const StoryTemplate: Story = {
+const IndeterminateTemplate: Story = {
   args: {},
   render: (args) => (
     <VariantTable Component={meta.component} variantMap={progressCircleVariantMap} {...args} />
   ),
 };
 
-export const LightTheme = StoryTemplate;
+const Determinate0Template: Story = {
+  args: {
+    value: 0,
+  },
+  render: (args) => (
+    <VariantTable Component={meta.component} variantMap={progressCircleVariantMap} {...args} />
+  ),
+};
 
-export const DarkTheme = StoryTemplate;
+const Determinate50Template: Story = {
+  args: {
+    value: 50,
+  },
+  render: (args) => (
+    <VariantTable Component={meta.component} variantMap={progressCircleVariantMap} {...args} />
+  ),
+};
+
+const Determinate100Template: Story = {
+  args: {
+    value: 100,
+  },
+  render: (args) => (
+    <VariantTable Component={meta.component} variantMap={progressCircleVariantMap} {...args} />
+  ),
+};
+
+export const IndeterminateLightTheme = IndeterminateTemplate;
+
+export const IndeterminateDarkTheme = IndeterminateTemplate;
+
+export const Determinate0LightTheme = Determinate0Template;
+
+export const Determinate0DarkTheme = Determinate0Template;
+
+export const Determinate50LightTheme = Determinate50Template;
+
+export const Determinate50DarkTheme = Determinate50Template;
+
+export const Determinate100LightTheme = Determinate100Template;
+
+export const Determinate100DarkTheme = Determinate100Template;
