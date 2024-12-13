8677ffb424e33e4882c542a3fdfea0e8e64ea4f4
malangcat <asher@daangn.com>
2024-12-13 22:33:35 +0900

    fix: badge examples

---
+++ docs/components/example/badge-large.tsx
@@ -0,0 +1,5 @@
+import { Badge } from "seed-design/ui/badge";
+
+export default function BadgeLarge() {
+  return <Badge size="large">라벨</Badge>;
+}
--- docs/components/example/badge-small.tsx
+++
@@ -1,5 +0,0 @@
-import { Badge } from "seed-design/ui/badge";
-
-export default function BadgeSmall() {
-  return <Badge size="small">라벨</Badge>;
-}
