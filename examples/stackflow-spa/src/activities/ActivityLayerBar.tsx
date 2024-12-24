488d17fccd291d3c622cbfe1896366f079b7e636
malangcat <asher@daangn.com>
2024-12-25 05:05:43 +0900

    chore: add example

---
+++ examples/stackflow-spa/src/activities/ActivityLayerBar.tsx
@@ -0,0 +1,48 @@
+import type { ActivityComponentType } from "@stackflow/react";
+import {
+  AppBar,
+  BackButton,
+  IconButton,
+  Left,
+  Right,
+  Title,
+} from "../design-system/stackflow/AppBar";
+import { AppScreen } from "../design-system/stackflow/AppScreen";
+
+import { IconBellLine } from "@daangn/react-monochrome-icon";
+import img from "../assets/peng.jpeg";
+import { theme } from "../stackflow/theme";
+
+const ActivityLayerBar: ActivityComponentType = () => {
+  return (
+    <AppScreen
+      appBar={
+        <AppBar tone="layer" border={false}>
+          <Left>
+            <BackButton />
+          </Left>
+          <Title>야옹</Title>
+          <Right>
+            <IconButton>
+              <IconBellLine />
+            </IconButton>
+            <IconButton>
+              <IconBellLine />
+            </IconButton>
+            <IconButton>
+              <IconBellLine />
+            </IconButton>
+            <IconButton>
+              <IconBellLine />
+            </IconButton>
+          </Right>
+        </AppBar>
+      }
+      theme={theme}
+    >
+      <div style={{ height: 800 }} />
+    </AppScreen>
+  );
+};
+
+export default ActivityLayerBar;
