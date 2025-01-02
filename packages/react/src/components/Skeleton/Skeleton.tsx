c983928b44b55c576b9d69f6ea4eedcbacd5306e
malangcat <asher@daangn.com>
2025-01-02 15:13:26 +0900

    fix: skeleton context

--- packages/react/src/components/Skeleton/Skeleton.tsx
+++ packages/react/src/components/Skeleton/Skeleton.tsx
@@ -3,11 +3,11 @@ import type * as React from "react";
 import { createStyleContext } from "../../utils/createStyleContext";
 import { Box, type BoxProps } from "../Box";
 
-const { withContext } = createStyleContext(skeleton);
+const { withProvider } = createStyleContext(skeleton);
 
 export interface SkeletonProps
   extends SkeletonVariantProps,
     Pick<BoxProps, "width" | "height">,
     Omit<React.HTMLAttributes<HTMLDivElement>, "color"> {}
 
-export const Skeleton = withContext<HTMLDivElement, SkeletonProps>(Box, "root");
+export const Skeleton = withProvider<HTMLDivElement, SkeletonProps>(Box, "root");
