88a53f02a809e561046fbabd67c3bdd7fadf9bbb
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-01-10 18:51:38 +0900

    docs: Text Field 중복 예시 제거

--- docs/components/example/text-field-indicator.tsx
+++ docs/components/example/text-field-indicator.tsx
@@ -1,8 +1,8 @@
 import { TextField, TextFieldInput } from "seed-design/ui/text-field";
 
-export default function TextFieldRequired() {
+export default function TextFieldIndicator() {
   return (
-    <TextField label="라벨" description="설명을 써주세요" required indicator="(필수)">
+    <TextField label="라벨" description="설명을 써주세요" indicator="(선택)">
       <TextFieldInput placeholder="플레이스홀더" />
     </TextField>
   );
