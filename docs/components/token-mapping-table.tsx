218904a0f343e49781e6233110d694bbb2512403
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-01-10 14:59:14 +0900

    docs: add color & typography mapping tables

---
+++ docs/components/token-mapping-table.tsx
@@ -0,0 +1,85 @@
+import {
+  resolveToken,
+  stringifyValueExpression,
+  type TokenRef,
+  type ValueExpression,
+} from "@seed-design/rootage-core";
+import { getRootage } from "@/components/get-rootage";
+import { ExpandableTokenCell } from "@/components/expandable-token-cell";
+
+interface TokenMapping {
+  previousTokenId: string;
+  newTokenIds: TokenRef[];
+  note?: string;
+}
+
+interface TokenMappingTableProps {
+  mappings: TokenMapping[];
+}
+
+export interface TokenMappingItem {
+  previousTokenId: TokenMapping["previousTokenId"];
+  newTokens: {
+    id: TokenMapping["newTokenIds"][number];
+    values: string[];
+    resolvedValue: ValueExpression;
+  }[];
+  note?: TokenMapping["note"];
+}
+
+export async function TokenMappingTable({ mappings }: TokenMappingTableProps) {
+  const rootage = await getRootage();
+
+  const tableItems: TokenMappingItem[] = mappings.map((mapping) => ({
+    previousTokenId: mapping.previousTokenId,
+    newTokens: mapping.newTokenIds.map((newId) => {
+      const { path, value } = resolveToken(rootage, newId, {
+        global: "default",
+        color: "theme-light",
+      });
+
+      return {
+        id: newId,
+        values: [...path, stringifyValueExpression(value)],
+        resolvedValue: value,
+      };
+    }),
+    note: mapping.note,
+  }));
+
+  return (
+    <table>
+      <colgroup>
+        <col />
+        <col />
+        <col style={{ width: "15%" }} />
+      </colgroup>
+      <thead>
+        <tr>
+          <th>V2</th>
+          <th>V3</th>
+          <th>비고</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableItems.map((item) => (
+          <tr key={item.previousTokenId}>
+            <td>{item.previousTokenId}</td>
+            <td className="align-middle space-y-2">
+              {item.newTokens.length > 0 &&
+                item.newTokens.map((newToken, index) => (
+                  <>
+                    <ExpandableTokenCell key={newToken.id} newToken={newToken} />
+                    {index !== item.newTokens.length - 1 && (
+                      <div className="text-xs text-center">또는</div>
+                    )}
+                  </>
+                ))}
+            </td>
+            <td>{item.note}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
