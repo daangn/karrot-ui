2239a0700712fb3a8ad72c7640f665b1000d2b09
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-01-20 12:00:21 +0900

    feat(figma-codegen): Text generate 시 불필요한 comment 제거

--- tools/figma-codegen/src/codegen/text.ts
+++ tools/figma-codegen/src/codegen/text.ts
@@ -1,14 +1,5 @@
 import { getTypographyVariableName } from "./variable";
 
-export function getTextStyleTag(textStyle: TextStyle) {
-  if (textStyle.name.startsWith("heading")) return "h1";
-  if (textStyle.name.startsWith("title")) return "h2";
-  if (textStyle.name.startsWith("body")) return "p";
-  if (textStyle.name.startsWith("label")) return "span";
-
-  return "span";
-}
-
 export function createTextProps(
   segmentBoundVariables: ReturnType<TextNode["getStyledTextSegments"]>[number]["boundVariables"],
 ) {
