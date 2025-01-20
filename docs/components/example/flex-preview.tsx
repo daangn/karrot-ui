58e9d5ce4f4be7072c0827bfe680c10f811e4c80
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-01-20 12:48:27 +0900

    docs: Layout 컴포넌트 문서 개선

--- docs/components/example/flex-preview.tsx
+++ docs/components/example/flex-preview.tsx
@@ -2,15 +2,41 @@ import { Flex } from "seed-design/ui/layout";
 
 export default function FlexPreview() {
   return (
-    <Flex direction="column" background="bg.dangerSolid" gap="s4" width="full">
-      <Flex background="bg.neutralWeak" paddingX="s4" paddingY="s3">
-        1
+    <Flex direction="row" background="bg.layerDefault" gap="s2" width="full" borderRadius="s2">
+      <Flex
+        direction="column"
+        background="bg.brandSolid"
+        gap="s1_5"
+        paddingX="s2"
+        paddingY="s2"
+        flexGrow={1}
+        borderRadius="s2"
+      >
+        <Flex background="bg.neutralWeak" paddingX="s4" paddingY="s3" borderRadius="s1">
+          1
+        </Flex>
+        <Flex background="bg.neutralWeak" paddingX="s4" paddingY="s3" borderRadius="s1">
+          2
+        </Flex>
       </Flex>
-      <Flex background="bg.neutralWeak" paddingX="s4" paddingY="s3">
-        2
+      <Flex
+        direction="row"
+        background="bg.brandSolid"
+        gap="s1_5"
+        paddingX="s2"
+        paddingY="s2"
+        flexGrow={1}
+        borderRadius="s2"
+      >
+        <Flex background="bg.neutralWeak" paddingX="s4" paddingY="s3" borderRadius="s1">
+          3
+        </Flex>
+        <Flex background="bg.neutralWeak" paddingX="s4" paddingY="s3" borderRadius="s1">
+          4
+        </Flex>
       </Flex>
-      <Flex background="bg.neutralWeak" paddingX="s4" paddingY="s3">
-        3
+      <Flex background="bg.brandSolid" paddingX="s4" paddingY="s3" borderRadius="s2">
+        5
       </Flex>
     </Flex>
   );
