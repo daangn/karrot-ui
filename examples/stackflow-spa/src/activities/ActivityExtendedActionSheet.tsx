5f5ac2c26bcac52f5b34bd2a91e974598cb9593b
malangcat <asher@daangn.com>
2025-01-20 21:53:56 +0900

    chore: update example

--- examples/stackflow-spa/src/activities/ActivityExtendedActionSheet.tsx
+++ examples/stackflow-spa/src/activities/ActivityExtendedActionSheet.tsx
@@ -1,5 +1,6 @@
 import { useActivity, type ActivityComponentType } from "@stackflow/react";
 
+import { IconPencilLine, IconPlusLine, IconTrashcanLine } from "@daangn/react-monochrome-icon";
 import {
   ExtendedActionSheetContent,
   ExtendedActionSheetGroup,
@@ -35,13 +36,34 @@ const ActivityExtendedActionSheet: ActivityComponentType = () => {
     <ExtendedActionSheetRoot open={activity.isActive} onOpenChange={handleClose}>
       <ExtendedActionSheetContent title="Actions" layerIndex={activity.zIndex * 5}>
         <ExtendedActionSheetGroup>
-          <ExtendedActionSheetItem onClick={handleAction("add")} label="Add" />
-          <ExtendedActionSheetItem onClick={handleAction("edit")} label="Edit" />
+          <ExtendedActionSheetItem
+            onClick={handleAction("add")}
+            prefixIcon={<IconPlusLine />}
+            label="Add"
+          />
+          <ExtendedActionSheetItem
+            onClick={handleAction("edit")}
+            prefixIcon={<IconPencilLine />}
+            label="Edit"
+          />
         </ExtendedActionSheetGroup>
         <ExtendedActionSheetGroup>
-          <ExtendedActionSheetItem onClick={handleAction("test1")} label="test1" />
-          <ExtendedActionSheetItem onClick={handleAction("test2")} label="tests" />
-          <ExtendedActionSheetItem onClick={handleAction("delete")} tone="danger" label="Delete" />
+          <ExtendedActionSheetItem
+            onClick={handleAction("test1")}
+            prefixIcon={<IconPlusLine />}
+            label="Test1"
+          />
+          <ExtendedActionSheetItem
+            onClick={handleAction("test2")}
+            prefixIcon={<IconPlusLine />}
+            label="Test2"
+          />
+          <ExtendedActionSheetItem
+            onClick={handleAction("delete")}
+            tone="danger"
+            prefixIcon={<IconTrashcanLine />}
+            label="Delete"
+          />
         </ExtendedActionSheetGroup>
       </ExtendedActionSheetContent>
     </ExtendedActionSheetRoot>
