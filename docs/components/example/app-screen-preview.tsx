10ea9d19f5fb1ceaf056bd6f9684ed3a20090f70
malangcat <asher@daangn.com>
2025-01-28 02:15:52 +0900

    fix: documentation examples

--- docs/components/example/app-screen-preview.tsx
+++ docs/components/example/app-screen-preview.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import { Flex } from "@/registry/ui/layout";
 import { IconBellFill } from "@daangn/react-monochrome-icon";
 import type { ActivityComponentType } from "@stackflow/react/future";
 import {
   AppBar,
-  AppScreen,
-  CloseButton,
-  IconButton,
-  Left,
-  Right,
-  Title,
-} from "seed-design/ui/app-screen";
+  AppBarCloseButton,
+  AppBarIconButton,
+  AppBarLeft,
+  AppBarRight,
+  AppBarTitle,
+} from "seed-design/ui/app-bar";
+import { AppScreen, AppScreenContent } from "seed-design/ui/app-screen";
+import { Flex } from "seed-design/ui/layout";
 
 declare module "@stackflow/config" {
   interface Register {
@@ -21,25 +21,23 @@ declare module "@stackflow/config" {
 
 const AppScreenPreviewActivity: ActivityComponentType<"AppScreenPreview"> = () => {
   return (
-    <AppScreen
-      theme="cupertino"
-      appBar={
-        <AppBar>
-          <Left>
-            <CloseButton />
-          </Left>
-          <Title>Preview</Title>
-          <Right>
-            <IconButton aria-label="Notification">
-              <IconBellFill />
-            </IconButton>
-          </Right>
-        </AppBar>
-      }
-    >
-      <Flex height="full" justifyContent="center" alignItems="center">
-        Preview
-      </Flex>
+    <AppScreen theme="cupertino">
+      <AppBar>
+        <AppBarLeft>
+          <AppBarCloseButton />
+        </AppBarLeft>
+        <AppBarTitle>Preview</AppBarTitle>
+        <AppBarRight>
+          <AppBarIconButton aria-label="Notification">
+            <IconBellFill />
+          </AppBarIconButton>
+        </AppBarRight>
+      </AppBar>
+      <AppScreenContent>
+        <Flex height="full" justifyContent="center" alignItems="center">
+          Preview
+        </Flex>
+      </AppScreenContent>
     </AppScreen>
   );
 };
