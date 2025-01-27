c07d320987e4dfd2e85d8bb418f12d4060ed3ba9
malangcat <asher@daangn.com>
2025-01-28 02:06:24 +0900

    fix: appbar composition

--- packages/stackflow/src/components/AppBar/AppBar.tsx
+++ packages/stackflow/src/components/AppBar/AppBar.tsx
@@ -1,12 +1,12 @@
 import { Slot } from "@radix-ui/react-slot";
+import { mergeProps } from "@seed-design/dom-utils";
+import { Primitive, type PrimitiveProps } from "@seed-design/react-primitive";
 import { type AppBarVariantProps, appBar } from "@seed-design/recipe/appBar";
 import clsx from "clsx";
 import { forwardRef } from "react";
 import { AppBar as AppBarPrimitive } from "../../primitive";
 import { useAppBarContext } from "../../primitive/AppBar/useAppBarContext";
 import { createStyleContext } from "../../utils/createStyleContext";
-import { mergeProps } from "@seed-design/dom-utils";
-import { Primitive } from "@seed-design/react-primitive";
 
 const { PropsProvider, withProvider, withContext, useClassNames } = createStyleContext(appBar);
 
@@ -37,28 +37,36 @@ export const AppBarTitle = withContext<HTMLDivElement, AppBarTitleProps>(
   "title",
 );
 
-export interface AppBarTitleMainProps extends React.HTMLAttributes<HTMLDivElement> {}
+export interface AppBarTitleMainProps
+  extends PrimitiveProps,
+    React.HTMLAttributes<HTMLDivElement> {}
 
 export const AppBarTitleMain = withContext<HTMLDivElement, AppBarTitleMainProps>(
   Primitive.div,
   "titleMain",
 );
 
-export interface AppBarTitleTextProps extends React.HTMLAttributes<HTMLSpanElement> {}
+export interface AppBarTitleTextProps
+  extends PrimitiveProps,
+    React.HTMLAttributes<HTMLSpanElement> {}
 
 export const AppBarTitleText = withContext<HTMLSpanElement, AppBarTitleTextProps>(
   Primitive.span,
   "titleText",
 );
 
-export interface AppBarSubtitleTextProps extends React.HTMLAttributes<HTMLSpanElement> {}
+export interface AppBarSubtitleTextProps
+  extends PrimitiveProps,
+    React.HTMLAttributes<HTMLSpanElement> {}
 
 export const AppBarSubtitleText = withContext<HTMLSpanElement, AppBarSubtitleTextProps>(
   Primitive.span,
   "subtitleText",
 );
 
-export interface AppBarIconButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+export interface AppBarIconButtonProps
+  extends PrimitiveProps,
+    React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export const AppBarIconButton = forwardRef<HTMLButtonElement, AppBarIconButtonProps>(
   ({ children, className, ...otherProps }, ref) => {
@@ -66,7 +74,7 @@ export const AppBarIconButton = forwardRef<HTMLButtonElement, AppBarIconButtonPr
     const classNames = useClassNames();
 
     return (
-      <button
+      <Primitive.button
         ref={ref}
         type="button"
         className={clsx(classNames.iconButton, className)}
@@ -75,7 +83,7 @@ export const AppBarIconButton = forwardRef<HTMLButtonElement, AppBarIconButtonPr
         <Slot className={classNames.icon} {...stateProps}>
           {children}
         </Slot>
-      </button>
+      </Primitive.button>
     );
   },
 );
