d9ed89399a1966fd83b5c60b9a051994b4168102
malangcat <asher@daangn.com>
2025-02-05 21:11:56 +0900

    docs: pass down example-only styles

--- docs/components/example-frame.tsx
+++ docs/components/example-frame.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import "@seed-design/stylesheet/base.css";
+import "@seed-design/stylesheet/global.css";
+import "@seed-design/stylesheet/token.css";
+import "@seed-design/stylesheet/component.min.css";
+import "@stackflow/plugin-basic-ui/index.css";
+import "simple-reveal/index.css";
+
 import clsx from "clsx";
 import { useEffect, useRef, useState } from "react";
 import type ReactDOM from "react-dom/client";
@@ -25,6 +32,8 @@ export default function ExampleFrame({ name }: ExampleFrameProps) {
 
     const styles = Array.from(document.styleSheets)
       .map((sheet) => {
+        // FIXME: ugly hack to filter out example-frame styles; should be improved to separately bundle example styles
+        if (!sheet.href?.includes("page.css")) return "";
         try {
           return Array.from(sheet.cssRules)
             .map((rule) => rule.cssText)
@@ -44,9 +53,17 @@ export default function ExampleFrame({ name }: ExampleFrameProps) {
           #root {
             width: 100vw;
             height: 100vh;
+            box-sizing: border-box;
             display: flex;
             justify-content: center;
             align-items: center;
+
+            font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+            Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+          }
+          body {
+            margin: 0;
+            padding: 0;
           }
         </style>
       </head>
