2dcabe8ba0f122d0b39f56c1f44be430dda55f3e
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2025-02-06 18:16:39 +0900

    feat: rollback iframe

--- docs/components/component-example.tsx
+++ docs/components/component-example.tsx
@@ -1,10 +1,9 @@
 import { Tab, Tabs } from "fumadocs-ui/components/tabs";
 import * as React from "react";
-
 import * as Index from "./example/index.json";
-import { CodeBlock } from "./code-block";
+
 import ErrorBoundary from "./error-boundary";
-import ExampleFrame from "./example-frame";
+import { CodeBlock } from "./code-block";
 
 interface ComponentExampleProps {
   name: string;
@@ -15,6 +14,16 @@ interface ComponentExampleProps {
 export function ComponentExample(props: ComponentExampleProps) {
   const { name } = props;
 
+  const Preview = React.useMemo(() => {
+    const Component = React.lazy(() => import(`./example/${name}.tsx`));
+
+    if (!Component) {
+      return <div>컴포넌트가 존재하지 않습니다.</div>;
+    }
+
+    return <Component />;
+  }, [name]);
+
   const Code = React.useMemo(() => {
     return (Index as Record<string, string>)[name];
   }, [name]);
@@ -22,7 +31,23 @@ export function ComponentExample(props: ComponentExampleProps) {
   if (props.previewOnly) {
     return (
       <React.Suspense fallback={null}>
-        <ExampleFrame name={name} />
+        <div
+          className="not-prose example-reset"
+          style={{
+            minHeight: "300px",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            backgroundColor: "var(--seed-v3-color-bg-layer-default)",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "10px",
+            borderRadius: ".75rem",
+            border: "1px solid var(--seed-v3-color-stroke-neutral)",
+          }}
+        >
+          {Preview}
+        </div>
       </React.Suspense>
     );
   }
@@ -32,7 +57,22 @@ export function ComponentExample(props: ComponentExampleProps) {
       <Tabs items={["미리보기", "코드"]}>
         <Tab value="미리보기">
           <React.Suspense fallback={null}>
-            <ExampleFrame name={name} />
+            <div
+              className="not-prose example-reset"
+              style={{
+                minHeight: "300px",
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+                backgroundColor: "var(--seed-v3-color-bg-layer-default)",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: "10px",
+                borderRadius: ".75rem",
+              }}
+            >
+              {Preview}
+            </div>
           </React.Suspense>
         </Tab>
         <Tab value="코드">
