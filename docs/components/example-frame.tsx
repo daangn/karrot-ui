0fd12aab4194d63b765e1542e23ee7ea0810e365
malangcat <asher@daangn.com>
2025-02-06 13:05:08 +0900

    Revert "docs: pass down example-only styles"
    This reverts commit d9ed89399a1966fd83b5c60b9a051994b4168102.

--- docs/components/example-frame.tsx
+++ docs/components/example-frame.tsx
@@ -1,12 +1,5 @@
 "use client";
 
-import "@seed-design/stylesheet/base.css";
-import "@seed-design/stylesheet/global.css";
-import "@seed-design/stylesheet/token.css";
-import "@seed-design/stylesheet/component.min.css";
-import "@stackflow/plugin-basic-ui/index.css";
-import "simple-reveal/index.css";
-
 import clsx from "clsx";
 import { useEffect, useRef, useState } from "react";
 import type ReactDOM from "react-dom/client";
@@ -32,8 +25,6 @@ export default function ExampleFrame({ name }: ExampleFrameProps) {
 
     const styles = Array.from(document.styleSheets)
       .map((sheet) => {
-        // FIXME: ugly hack to filter out example-frame styles; should be improved to separately bundle example styles
-        if (!sheet.href?.includes("page.css")) return "";
         try {
           return Array.from(sheet.cssRules)
             .map((rule) => rule.cssText)
@@ -53,17 +44,9 @@ export default function ExampleFrame({ name }: ExampleFrameProps) {
           #root {
             width: 100vw;
             height: 100vh;
-            box-sizing: border-box;
             display: flex;
             justify-content: center;
             align-items: center;
-
-            font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-            Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-          }
-          body {
-            margin: 0;
-            padding: 0;
           }
         </style>
       </head>
