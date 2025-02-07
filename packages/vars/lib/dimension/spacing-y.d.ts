1b1f4d95a3f52f0ad00e8297c4dfb7baadba9588
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-02-07 15:40:17 +0900

    feat(rootage): add token `$dimension.spacing-y.nav-to-title`

--- packages/vars/lib/dimension/spacing-y.d.ts
+++ packages/vars/lib/dimension/spacing-y.d.ts
@@ -1,1 +1,2 @@
 export declare const componentDefault = "var(--seed-v3-dimension-spacing-y-component-default)";
+export declare const navToTitle = "var(--seed-v3-dimension-spacing-y-nav-to-title)";
--- packages/vars/lib/dimension/spacing-y.mjs
+++ packages/vars/lib/dimension/spacing-y.mjs
@@ -1,1 +1,2 @@
 export const componentDefault = "var(--seed-v3-dimension-spacing-y-component-default)";
+export const navToTitle = "var(--seed-v3-dimension-spacing-y-nav-to-title)";
