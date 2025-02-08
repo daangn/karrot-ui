ddbb2b5bbf3146b97b67904a80937c565d0ef6dc
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2025-02-08 11:27:07 +0900

    chore(cli): change dev.mjs to watch

--- packages/cli/dev.mjs
+++ packages/cli/dev.mjs
@@ -1,22 +1,20 @@
-import esbuild from "esbuild";
+import { context } from "esbuild";
 
 import pkg from "./package.json" assert { type: "json" };
 
-esbuild
-  .build({
-    entryPoints: ["./src/index.ts"],
-    outfile: "./bin/index.mjs",
-    bundle: true,
-    write: true,
-    treeShaking: true,
-    sourcemap: false,
-    minify: true,
-    format: "esm",
-    platform: "node",
-    target: ["esnext"],
-    external: [...Object.keys(pkg.dependencies), "__temp.mjs"],
-    define: {
-      "process.env.NODE_ENV": `"dev"`,
-    },
-  })
-  .catch(() => process.exit(1));
+context({
+  entryPoints: ["./src/index.ts"],
+  outfile: "./bin/index.mjs",
+  bundle: true,
+  write: true,
+  treeShaking: true,
+  sourcemap: false,
+  minify: true,
+  format: "esm",
+  platform: "node",
+  target: ["esnext"],
+  external: [...Object.keys(pkg.dependencies), "__temp.mjs"],
+  define: {
+    "process.env.NODE_ENV": `"dev"`,
+  },
+}).catch(() => process.exit(1));
