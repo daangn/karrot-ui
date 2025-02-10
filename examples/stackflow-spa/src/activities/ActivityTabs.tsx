699b1fdded0768d410fbfc24860d159e0f66b9aa
malangcat <asher@daangn.com>
2025-02-10 12:00:51 +0900

    chore: update example

--- examples/stackflow-spa/src/activities/ActivityTabs.tsx
+++ examples/stackflow-spa/src/activities/ActivityTabs.tsx
@@ -20,27 +20,25 @@ const ActivityTabs: ActivityComponentType = () => {
         <AppBarMain title="Tabs" />
       </AppBar>
       <AppScreenContent>
-        <div style={{ height: "100%", overflowY: "auto" }}>
-          <Box padding="s4">Sticky Tablist</Box>
-          <TabsRoot stickyList defaultValue="1" triggerLayout="hug" contentLayout="hug">
-            <TabsList>
-              <TabsTrigger value="1">Tab 1</TabsTrigger>
-              <TabsTrigger value="2">Tab 2</TabsTrigger>
-            </TabsList>
-            <TabsContent value="1">
-              <Box borderWidth={1} borderColor="stroke.neutral" padding="s2" height="1000px">
-                Tab 1 content
+        <Box padding="s4">Sticky Tablist</Box>
+        <TabsRoot stickyList defaultValue="1" triggerLayout="hug" contentLayout="hug">
+          <TabsList>
+            <TabsTrigger value="1">Tab 1</TabsTrigger>
+            <TabsTrigger value="2">Tab 2</TabsTrigger>
+          </TabsList>
+          <TabsContent value="1">
+            <Box borderWidth={1} borderColor="stroke.neutral" padding="s2" height="1000px">
+              Tab 1 content
+            </Box>
+          </TabsContent>
+          <TabsContent value="2">
+            <AsyncContent>
+              <Box borderWidth={1} borderColor="stroke.neutral" paddingX="s2" paddingY="s10">
+                Tab 2 content
               </Box>
-            </TabsContent>
-            <TabsContent value="2">
-              <AsyncContent>
-                <Box borderWidth={1} borderColor="stroke.neutral" paddingX="s2" paddingY="s10">
-                  Tab 2 content
-                </Box>
-              </AsyncContent>
-            </TabsContent>
-          </TabsRoot>
-        </div>
+            </AsyncContent>
+          </TabsContent>
+        </TabsRoot>
       </AppScreenContent>
     </AppScreen>
   );
