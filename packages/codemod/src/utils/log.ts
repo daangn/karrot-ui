11306a067034e5ee0c79225ffb69c9fd25af6060
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2025-02-13 16:44:09 +0900

    chore: remove `@june-so/analytics-node`

--- packages/codemod/src/utils/log.ts
+++ packages/codemod/src/utils/log.ts
@@ -1,27 +1,13 @@
-import { Analytics, type TrackParams as JuneTrackParams } from "@june-so/analytics-node";
 import colors from "picocolors";
 
 export const LOG_PREFIX = colors.bold("@seed-design/codemod");
 
 interface TrackParams {
   event: string;
-  properties: JuneTrackParams["properties"];
+  properties: Record<string, unknown>;
 }
 
 export const createTrack = (metadata?: Record<string, unknown>) => {
-  if (process.env.PUBLIC_JUNE_SO_WRITE_KEY === undefined) return null;
-
-  const analytics = new Analytics(process.env.PUBLIC_JUNE_SO_WRITE_KEY);
-  const userId = process.env.USER ?? "unknown";
-
   return ({ event, properties }: TrackParams) =>
-    analytics.track({
-      event: `@seed-design/codemod: ${event}`,
-      userId,
-      properties: {
-        cwd: process.cwd(),
-        ...metadata,
-        ...properties,
-      },
-    });
+    console.log(LOG_PREFIX, event, properties, metadata);
 };
