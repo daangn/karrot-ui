36de5edb8d8cae6b0953eea11e9a3a18c6c26c68
malangcat <asher@daangn.com>
2025-02-15 18:49:52 +0900

    docs: fix spacing between title and content
    - PortableContent should be direct child of DocsBody to prose work correctly

--- docs/app/docs/design/[[...slug]]/page.tsx
+++ docs/app/docs/design/[[...slug]]/page.tsx
@@ -56,11 +56,8 @@ export default async function Page({
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
-        {guideline && (
-          <div className="mb-8">
-            <PortableContent content={guideline.content} />
-          </div>
-        )}
+        {guideline && <PortableContent content={guideline.content} />}
+        <div className="h-8" />
         <MDX components={mdxComponents} />
       </DocsBody>
     </DocsPage>
