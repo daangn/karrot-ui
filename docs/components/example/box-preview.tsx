7797a7955ea588fc1307b3a473e6c8ed77fa7102
malangcat <asher@daangn.com>
2025-02-15 20:49:00 +0900

    docs: fix box example

--- docs/components/example/box-preview.tsx
+++ docs/components/example/box-preview.tsx
@@ -3,7 +3,7 @@ import { Box } from "seed-design/ui/layout";
 export default function BoxPreview() {
   return (
     <Box
-      background="bg.brandWeak"
+      background="bg.neutralWeak"
       borderWidth={2}
       borderColor="stroke.brand"
       borderRadius="r2"
