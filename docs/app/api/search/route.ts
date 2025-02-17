505e4eea580cb57d7febb89cdce20140b53213ca
malangcat <asher@daangn.com>
2025-02-17 23:38:45 +0900

    wip korean search

--- docs/app/api/search/route.ts
+++ docs/app/api/search/route.ts
@@ -1,16 +1,61 @@
 import { source } from "@/app/source";
-import { createFromSource } from "fumadocs-core/search/server";
+import { tokenizer } from "@orama/orama/components";
+import { AdvancedIndex, createSearchAPI } from "fumadocs-core/search/server";
 
 // it should be cached forever
 export const revalidate = false;
 
-export const { staticGET: GET } = createFromSource(source, (page) => {
-  return {
-    title: page.data.title,
-    description: page.data.description,
-    url: page.url,
-    id: page.url,
-    structuredData: page.data.structuredData,
-    tag: page.slugs[0],
-  };
+const defaultTokenizer = await tokenizer.createTokenizer({
+  language: "english",
+});
+
+const normalizeToken = tokenizer.normalizeToken.bind(defaultTokenizer, "");
+
+function trim(text: string[]): string[] {
+  while (text[text.length - 1] === "") {
+    text.pop();
+  }
+  while (text[0] === "") {
+    text.shift();
+  }
+  return text;
+}
+
+function tokenize(input: string): string[] {
+  if (typeof input !== "string") {
+    return [input];
+  }
+
+  const splitRule = /[^ㄱ-ㅎ|ㅏ-ㅣ|가-힣|A-Za-zàèéìòóù0-9_'-]+/gim;
+  const tokens = input
+    .toLowerCase()
+    .split(splitRule)
+    .map((t) => normalizeToken(t))
+    .filter(Boolean);
+
+  const trimTokens = trim(tokens);
+
+  return Array.from(new Set(trimTokens));
+}
+
+export const { staticGET: GET } = createSearchAPI("advanced", {
+  indexes: () =>
+    Promise.all(
+      source.getPages().map(async (page) => {
+        const { structuredData } = await page.data.load();
+
+        return {
+          id: page.url,
+          title: page.data.title,
+          description: page.data.description,
+          structuredData,
+          tag: page.slugs[0],
+          url: page.url,
+        } satisfies AdvancedIndex;
+      }),
+    ),
+  tokenizer: {
+    language: "english",
+    tokenize: (raw) => tokenize(raw),
+  },
 });
